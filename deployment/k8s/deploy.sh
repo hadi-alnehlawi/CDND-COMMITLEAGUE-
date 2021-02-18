f71de7b33ca76f998517b56ebefb35be6fc79ba4
Hadi <hadi.alnehlawi@gmail.com>
2021-02-19 00:24:27 +0400

    trigger CI

--- deployment/k8s/deploy.sh
+++ deployment/k8s/deploy.sh
@@ -1,4 +1,11 @@
-kubectl apply -f env-config.yml;
-kubectl apply -f env-secret.yml;
-# kubectl apply -f commitleague-deployment.yml;
-# kubectl apply -f commitleague-service.yml;
+## clean up
+# kubectl delete configmap env-config;
+# kubectl delete secret env-secret;
+
+## setup
+# kubectl apply -f ./env-config.yml;
+# kubectl apply -f ./env-secret.yml;
+kubectl apply -f ./commitleague-deployment.yml;
+kubectl apply -f ./commitleague-service.yml;
+kubectl apply -f ./reverseproxy-deployment.yml;
+kubectl apply -f ./reverseproxy-service.yml;
